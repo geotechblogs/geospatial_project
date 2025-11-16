cb95bb6d1b52592563a5ab1df1e641049d6ae9eb
Andres M. Hernandez <147552227+geotechblogs@users.noreply.github.com>
2025-11-16 19:58:06 +0100

    Update geoproject/main.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- geoproject/main.py
+++ geoproject/main.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
         "main:app",
         host=settings.host,
         port=settings.port,
-        log_level=settings.log_level.lower(),
+        reload=settings.reload,
         reload=True,
     )
